46d8cc7f98203c27361f2f7b15b6454c238119d3
lishubin <2697601945@qq.com>
2022-09-17 10:39:30 +0800

    增加base.html,3-5user.html

--- 3_1_1hello.py
+++ 3_1_1hello.py
@@ -3,9 +3,9 @@
 # @File : 3_1_1hello.py
 # @Software :PyCharm
 from flask import Flask,render_template
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-
+boostrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name)
+    return render_template('3-5user.html', name = name)
 
 
 if __name__ == '__main__':
--- 3_2hello.py
+++ 3_2hello.py
@@ -2,6 +2,15 @@
 # @Author: 李树斌
 # @File : 3_2hello.py
 # @Software :PyCharm
-from  flask_bootstrap import Bootstrap
-#..
+from flask_bootstrap import Bootstrap
+from flask import Flask,render_template
+app = Flask(__name__)
 boostrap = Bootstrap(app)
+
+@app.run('/user/<name>')
+def hello(name):
+    return render_template('3-5user.html')
+
+
+if __name__ == '__main__':
+    app.run()
